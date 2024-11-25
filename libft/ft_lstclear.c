e4d8cc07de0329638895de4b15fd69969c0b64aa
Enrico Giovanni Carbonaro <ecarbona@c2r5p2.42firenze.it>
2024-11-25 13:44:07 +0100

    aggiunti main

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: ecarbona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 11:22:36 by ecarbona          #+#    #+#             */
-/*   Updated: 2024/11/22 12:04:43 by ecarbona         ###   ########.fr       */
+/*   Updated: 2024/11/25 13:18:43 by ecarbona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,30 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	}
 	*lst = NULL;
 }
+// static void ft_del(void *content)
+// {
+// 	free (content);
+// }
+// int main()
+// {
+// 	t_list *head = NULL;
+// 	t_list *new_node;
+// 	t_list *new_node1;
+// 	t_list *temp;
+
+// 	char *content1 = "1";
+// 	char *content2 = "2";
+// 	new_node = ft_lstnew((void *)content1);
+// 	ft_lstadd_front(&head, new_node);
+// 	new_node1 = ft_lstnew((void *)content2);
+// 	ft_lstadd_front(&head, new_node1);
+// 	temp = head;
+// 	while(temp)
+// 	{
+// 		printf("%s\n", (char *)temp->content);
+// 		temp = temp->next;
+// 	}
+// 	ft_lstclear(&temp, ft_del);
+// 	if(!temp)
+// 		printf("cancellata");
+// }
