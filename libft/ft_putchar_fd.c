8e4530b5ac721c6ceb0d08284c9e04e03f5ccd0c
Enrico Giovanni Carbonaro <ecarbona@c2r5p2.42firenze.it>
2024-11-25 18:11:14 +0100

    test fd e test free in list

--- libft/ft_putchar_fd.c
+++ libft/ft_putchar_fd.c
@@ -6,13 +6,18 @@
 /*   By: ecarbona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 16:20:04 by ecarbona          #+#    #+#             */
-/*   Updated: 2024/11/19 11:27:35 by ecarbona         ###   ########.fr       */
+/*   Updated: 2024/11/25 17:20:14 by ecarbona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
 void	ft_putchar_fd(char c, int fd)
 {
 	write (fd, &c, 1);
 }
+// #include <fcntl.h>
+// int main()
+// {
+// 	int fd = open("prova_fd.txt", O_WRONLY);
+// 	ft_putchar_fd('c', fd);
+// }
