8575bdcdfc58d4795507e9a8f476aed7f85ad3fa
Enrico Giovanni Carbonaro <ecarbona@c1r4p10.42firenze.it>
2025-01-14 13:36:13 +0100

    minitalk

--- Milestone_2/minitalk/client_bonus.c
+++ Milestone_2/minitalk/client_bonus.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecarbona <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 12:10:30 by ecarbona          #+#    #+#             */
+/*   Updated: 2025/01/14 13:35:33 by ecarbona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
 void	put_signal(int pid, char *str)
@@ -5,24 +17,42 @@ void	put_signal(int pid, char *str)
 	int	i;
 	int	c;
 
-	i = 0;
+	i = -1;
 	while (*str)
 	{
 		c = *(str);
-		while (i < 8)
+		while (++i < 8)
 		{
 			if (c << i & 0b10000000)
-				kill(pid, SIGUSR1);
+			{
+				if (kill(pid, SIGUSR1) == -1)
+					return ((void)ft_printf("Unable to send Message"));
+			}
 			else
-				kill(pid, SIGUSR2);
-			i++;
+			{
+				if (kill(pid, SIGUSR2) == -1)
+					return ((void)ft_printf("Unable to send Message"));
+			}
 			usleep(100);
 		}
 		str++;
-		i = 0;
+		i = -1;
 	}
-	
 }
+
+// void	handler(int sig)
+// {
+// 	static int	message_printed = 0;
+
+// 	if (!message_printed)
+// 	{
+// 		if (sig == SIGUSR1)
+// 		{
+// 			ft_printf("The signal was received successfully.\n");
+// 			message_printed = 1;
+// 		}
+// 	}
+// }
 
 int	main(int argc, char **argv)
 {
--- Milestone_2/minitalk/minitalk.h
+++ Milestone_2/minitalk/minitalk.h
@@ -1,12 +1,11 @@
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "libft/libft.h"
 # include <bits/types/siginfo_t.h>
 # include <signal.h>
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
 # include <stdio.h>
-# include <sys/types.h>
+# include "libft/libft.h"
 
 #endif
