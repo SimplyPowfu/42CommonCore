fb8aa095540d118d6a66f0dfeba2a7d74b92818e
SimplyPowfu <carbonaroenricog@gmail.com>
2025-06-19 18:28:59 +0200

    CPP

--- milestone_4/cpp/cpp00/ex02/account.cpp
+++ milestone_4/cpp/cpp00/ex02/account.cpp
@@ -6,7 +6,7 @@
 /*   By: ecarbona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 17:53:14 by ecarbona          #+#    #+#             */
-/*   Updated: 2025/06/19 18:06:34 by ecarbona         ###   ########.fr       */
+/*   Updated: 2025/06/19 18:28:10 by ecarbona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,61 @@ int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
 int	Account::_totalNbWithdrawals = 0;
 int	Account::_totalNbDeposits = 0;
+
+int	Account::getNbAccounts( void )
+{
+	
+}
+
+int	Account::getTotalAmount( void )
+{
+	
+}
+
+int	Account::getNbDeposits( void )
+{
+	
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	
+}
+
+int		Account::checkAmount( void ) const
+{
+	
+}
+
+void	Account::displayAccountsInfos()
+{
+	
+}
+
+void	Account::displayStatus() const
+{
+	
+}
+
+void	Account::_displayTimestamp( void )
+{
+	
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	
+}
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	
+}
+
+Account::Account( int initial_deposit )
+{
+	
+}
+Account::~Account( void )
+{
+	
+}
