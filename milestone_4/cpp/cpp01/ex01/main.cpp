63f05d86f988a2880f4078f390586026616a88a2
SimplyPowfu <carbonaroenricog@gmail.com>
2025-06-28 17:54:38 +0200

    fix

--- milestone_4/cpp/cpp01/ex01/main.cpp
+++ milestone_4/cpp/cpp01/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecarbona <ecarbona@student.42firenze.it    +#+  +:+       +#+        */
+/*   By: ecarbona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 17:55:40 by ecarbona          #+#    #+#             */
-/*   Updated: 2025/06/27 18:49:36 by ecarbona         ###   ########.fr       */
+/*   Updated: 2025/06/28 17:53:18 by ecarbona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main()
 {
 	Zombie* horde;
-	int		num;
+	int		num = 0;
 	std::string	name;
 
 	std::cout << "zombie name: ";
@@ -23,6 +23,8 @@ int	main()
 	if (std::cin.eof())
 		return (0);
 	std::cout << "zombie num: ";
+	if (std::cin.eof())
+		return (0);
 	std::cin >> num;
 	horde = zombieHorde(num, name);
 	for (int i = 0; i < num; i++)
